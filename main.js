3fd40bdcb358bf6591ee54adb060dfdf987aed22
AmandaStahlberg <amanda.stahlberg@medieinstitutet.se>
2020-11-08 16:55:24 +0100

    add functions for the navbar

--- main.js
+++ main.js
@@ -6,15 +6,33 @@ function main() {
 }
 
 function addEventListeners() {
-    aboutMeButton();
+    clickAboutMe();
+    clickRefrences();
+    clickInterests();
+    moreAboutMeButton();
 }
 
-function aboutMeButton() {
+function clickAboutMe() {
+    const aboutMe = document.getElementById('circle1');
+    aboutMe.onclick =  window.location = '#aboutMeDiv';
+}
+
+function clickRefrences() {
+    const refrences = document.getElementById('circle2');
+    refrences.onclick =  window.location = '#refrencesDiv';
+}
+
+function clickInterests() {
+    const interests = document.getElementById('circle3');
+    interests.onclick =  window.location = '#interestsDiv';
+}
+
+function moreAboutMeButton() {
     const myButton = document.getElementById('aboutMeButton');
-    myButton.onclick = function() {youClickedMe()}
+    myButton.onclick = function() {readMoreAboutMe()}
 }
 
-function youClickedMe() {
+function readMoreAboutMe() {
     let infoBox = document.getElementById('infoBox');
     infoBox.innerText = "Jag är en glad och sprallig smålands-tjej från en liten sjöstad (läs: by) på Ostkusten. Bytte ifrån ena kusten till den andra och har varit bosatt i Göteborg dom senaste 3 åren och trivs väldigt bra. Har funnit ett intresse i programmering och studerar just nu på Medieinstitutet till Front End Utvecklare. Går sådär men jag kämpar på....";
     const myButton = document.getElementById('aboutMeButton');
@@ -25,3 +43,4 @@ function youClickedMe() {
 function youClickedMeAgain() {
     window.location.href = './index.html';
 }
+
