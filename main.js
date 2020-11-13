43e4ea65dd6a1bfd81045a07708a6fb6f76ccbb9
AmandaStahlberg <amanda.stahlberg@medieinstitutet.se>
2020-11-13 13:20:07 +0100

    add jsDoc

--- main.js
+++ main.js
@@ -1,20 +1,28 @@
 window.addEventListener('load', main)
 
-
+/**
+ * Function to load the js-file.
+ */
 function main() {
     addEventListeners()
 }
 
+/**
+ * Function to add eventListeners.
+ */
 function addEventListeners() {
     clickAboutMe()
     clickRefrences()
     clickInterests()
     clickTopButton()
     moreAboutMeButton()
-    enlargeImg()
+    getTheRightImg()
 }
 
-function enlargeImg() {
+/**
+ * Function to get the img from html.
+ */
+function getTheRightImg() {
     const images = document.querySelectorAll('.collage img')
     
     for (const img of images) {
@@ -22,8 +30,10 @@ function enlargeImg() {
     }
 }
 
-    
 
+/**
+ * Function to resize the img, both larger and smaller.
+ */
 function clickedImg(event) {
     const img = event.target
 
@@ -39,9 +49,10 @@ function clickedImg(event) {
         img.style.transition = "transform 0.25s ease"
     }
 }
-  
 
-
+/**
+ * Function to add onclick on the "aboutMe-buttons".
+ */
 function clickAboutMe() {
     const aboutMe = document.getElementById('circle1')
     const aboutMeFooter = document.getElementById('aboutMeFooter')
@@ -49,9 +60,16 @@ function clickAboutMe() {
     aboutMeFooter.onclick =  youClickedAboutMe
 }
 
+/**
+ * Function to get to the the div with informations about me.
+ */
 function youClickedAboutMe() {
     window.location = '#aboutMeDiv'
 }
+
+/**
+ * Function to add onclick on the "refrences-buttons".
+ */
 function clickRefrences() {
     const refrences = document.getElementById('circle3')
     const refrencesFooter = document.getElementById('refrencesFooter')
@@ -59,10 +77,16 @@ function clickRefrences() {
     refrencesFooter.onclick =  youClickedRefrences
 }
 
+/**
+ * Function to get to the the div with the refrences.
+ */
 function youClickedRefrences() {
     window.location = '#refrencesDiv'
 }
 
+/**
+ * Function to add onclick on the "interests-buttons".
+ */
 function clickInterests() {
     const interests = document.getElementById('circle2')
     const interestsFooter = document.getElementById('interestsFooter')
@@ -70,23 +94,39 @@ function clickInterests() {
     interestsFooter.onclick =  youClickedInterests
 }
 
+/**
+ * Function to get to the the div with the img-collage.
+ */
 function youClickedInterests() {
     window.location = '#interestsDiv'
 }
+
+/**
+ * Function to add onclick on the "top-button".
+ */
 function clickTopButton() {
     const topButton = document.getElementById('topButton')
     topButton.onclick =  youClickedTopButton
 }
 
+/**
+ * Function to get to the top of the website.
+ */
 function youClickedTopButton() {
     window.location = './index.html'
 }
 
+/**
+ * Function to add onclick on the "read more about me-button".
+ */
 function moreAboutMeButton() {
     const myButton = document.getElementById('aboutMeButton')
     myButton.onclick = readMoreAboutMe
 }
 
+/**
+ * Function to change the text in the info-box and on the button. + add onclick on the "go back-button"
+ */
 function readMoreAboutMe() {
     let infoBox = document.getElementById('infoBox')
     infoBox.innerText = "Jag är en glad och sprallig smålands-tjej från en liten sjöstad (läs: by) på Ostkusten. Bytte ifrån ena kusten till den andra och har varit bosatt i Göteborg dom senaste 3 åren och trivs väldigt bra. Har funnit ett intresse i programmering och studerar just nu på Medieinstitutet till Front End Utvecklare. Går sådär men jag kämpar på...."
@@ -96,6 +136,9 @@ function readMoreAboutMe() {
     myButton.onclick = youClickedMeAgain
 }
 
+/**
+ * Function to get back to the original text in infobox and button.
+ */
 function youClickedMeAgain() {
     const infoBox = document.getElementById('infoBox')
     infoBox.innerHTML = '<p> <b>Namn:</b> Amanda Ståhlberg <br> <b>Ålder:</b> 27år <br> <b>Bor:</b> Göteborg <br><b>Gör:</b> Just nu pluggar jag front end utvekling på Medieinstitutet. <br><b>Egenskaper:</b> Extremt envis = Åkt vasaloppet helt otränad!<br>& väldigt rolig enligt mig själv. Vem skrattar inte åt sina egna skämt.</p>'
